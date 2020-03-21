94d1be4bc54d1cdb46b5a2b5c4332b8bfc26584e
Dylan Law <dylanlaw123@gmail.com>
2020-03-20 23:18:28 -0700

    Capstone

--- Capstone/portfolio/readme.md
+++ Capstone/portfolio/readme.md
@@ -6,4 +6,6 @@ Relating to recent times, I would love to create a Coronavirus tracking website 
 
 I always found Ajax to be pretty interesting, so I choose to take what I learned from the Weather API Lab and apply it to the Coronavirus website.
 
+One thing that was rather difficult was getting accurate information on all the deathes, so I used mutiple websites to get the information from that would update quicker as before the https://covid19.mathdro.id/api/daily/ website I used before only updated at the end of the day.  I also had mutiple Ajax's for the mutiple websites I took the data from.  On the China and USA pages, there is updated data that updates more frequently.
+
 In terms of relating my learning to the code, it was quite difficult.  How the data was structured made it so I had to take extra procautions to make sure all the data was in the right place.  Such as grouping the data from the US and China by taking to Providence so I could make my own tabs to represent them.  The chart is similar to the icon as it takes the chart from the website I took the data from and displays it.  All in all, the projct was fun to do and I hope it shows what I have learned over the quarter regrading, HTML, Css, Jquery and AJAX. 
--- Capstone/site/js/index.js
+++ Capstone/site/js/index.js
@@ -1,67 +1,115 @@
+
+function formatDateTime(date) {
+  var year = date.getFullYear(),
+      month = date.getMonth() + 1,
+      day = date.getDate(),
+      hour = date.getHours(),
+      minute = date.getMinutes(),
+      hourFormatted = hour % 12 || 12,
+      minuteFormatted = minute < 10 ? "0" + minute : minute,
+      morning = hour < 12 ? "am" : "pm";
+
+  return month + "/" + day + "/" + year + " " + hourFormatted + ":" +
+          minuteFormatted + morning;
+}
+
 $(document).ready(function() {
   var today = new Date();
   var yesterday = new Date(today.setDate(today.getDate() - 1));
   let dateFormatted = (yesterday.getMonth() + 1) + "-" + yesterday.getDate() + "-" + yesterday.getFullYear()
   $('.yesterday').text(dateFormatted);
 
-  var covidUrl = "https://covid19.mathdro.id/api/daily/" + dateFormatted;
+  var covidDailyUrl = "https://covid19.mathdro.id/api/daily/" + dateFormatted;
+  var covidUSAUrl = "https://covid19.mathdro.id/api/countries/USA";
+  var covidChinaUrl = "https://covid19.mathdro.id/api/countries/China"; 
+
+  var resultDaily;
+  var resultUSA;
+  var resultChina;
+
+  $.when(
     $.ajax({
-      url: covidUrl,
-      success: function(data) {
-        var dataUS = data.filter(function (item) {
-          return (item.countryRegion === "US");
-        });
+        url: covidDailyUrl, 
+        success: function(data){     
+          resultDaily = data;                  
+        }           
+    }),
 
-        var dataChina = data.filter(function (item) {
-          return (item.countryRegion === "China");
-        });
+    $.ajax({
+      url: covidUSAUrl, 
+      success: function(data){     
+        resultUSA = data;                  
+      }           
+    }),
 
-        // Group the result of the world status by country
-        var resultWorld = [];
-        data.reduce(function(res, value) {
-            if (!res[value.countryRegion]){
-                res[value.countryRegion] = { countryRegion: value.countryRegion, confirmed: 0, deaths: 0, recovered: 0 };
-                resultWorld.push(res[value.countryRegion])
-              }
-            res[value.countryRegion].confirmed += parseInt(value.confirmed);
-            res[value.countryRegion].deaths += parseInt(value.deaths);
-            res[value.countryRegion].recovered += parseInt(value.recovered);
-            return res;
-        }, {});
+    $.ajax({
+      url: covidChinaUrl, 
+      success: function(data){     
+        resultChina = data;                  
+      }           
+    }) 
+  ).then(function() {
+    var data = resultDaily; 
+    var dataUS = data.filter(function (item) {
+      return (item.countryRegion === "US" && item.provinceState !== "US");
+    });
 
-        // Sort the result for world, us and China
-        var sortedResultWorld = resultWorld.slice(0);
-        sortedResultWorld.sort(function(a, b) {
-            return b.confirmed - a.confirmed;
-        })
+    var dataChina = data.filter(function (item) {
+      return (item.countryRegion === "China" && item.provinceState !== "China");
+    });
 
-        var sortedResultUS = dataUS.slice(0);
-        sortedResultUS.sort(function(a, b) {
-            return b.confirmed - a.confirmed;
-        })
+    // Group the result of the world status by country
+    var resultWorld = [];
+    data.reduce(function(res, value) {
+        if (!res[value.countryRegion]){
+            res[value.countryRegion] = { countryRegion: value.countryRegion, confirmed: 0, deaths: 0, recovered: 0 };
+            resultWorld.push(res[value.countryRegion])
+          }
+        res[value.countryRegion].confirmed += parseInt(value.confirmed);
+        res[value.countryRegion].deaths += parseInt(value.deaths);
+        res[value.countryRegion].recovered += parseInt(value.recovered);
+        return res;
+    }, {});
 
-        var sortedResultChina = dataChina.slice(0);
-        sortedResultChina.sort(function(a, b) {
-            return b.confirmed - a.confirmed;
-        })
-
-        // Display the result for world, us and China
-        var strTR = "";        
-        sortedResultWorld.forEach(function(item) {
-            strTR = "<tr><td>" + item.countryRegion + "</td><td>" + item.confirmed + "</td><td class='red'>" + item.deaths + "</td><td>" + item.recovered + "</td></tr>";
-            $('#world .tbody-data').append(strTR);
-        });
-     
-        sortedResultUS.forEach(function(item) {
-            strTR = "<tr><td>" + item.provinceState + "</td><td>" + item.confirmed + "</td><td class='red'>" + item.deaths + "</td></tr>";
-            $('#us .tbody-data').append(strTR);
-        });
-             
-        sortedResultChina.forEach(function(item) {
-            strTR = "<tr><td>" + item.provinceState + "</td><td>" + item.confirmed + "</td><td class='red'>" + item.deaths + "</td><td>" + item.recovered + "</td></tr>";
-            $('#china .tbody-data').append(strTR);
-        });         
-      }
+    // Sort the result for world, us and China
+    var sortedResultWorld = resultWorld.slice(0);
+    sortedResultWorld.sort(function(a, b) {
+        return b.confirmed - a.confirmed;
     })
-  }
-);
+
+    var sortedResultUS = dataUS.slice(0);
+    sortedResultUS.sort(function(a, b) {
+        return b.confirmed - a.confirmed;
+    })
+
+    var sortedResultChina = dataChina.slice(0);
+    sortedResultChina.sort(function(a, b) {
+        return b.confirmed - a.confirmed;
+    })
+
+    // Display the result for world, us and China
+    var strTR = "";        
+    sortedResultWorld.forEach(function(item) {
+        strTR = "<tr><td>" + item.countryRegion + "</td><td>" + item.confirmed + "</td><td class='red'>" + item.deaths + "</td><td>" + item.recovered + "</td></tr>";
+        $('#world .tbody-data').append(strTR);
+    });
+  
+    sortedResultUS.forEach(function(item) {
+        strTR = "<tr><td>" + item.provinceState + "</td><td>" + item.confirmed + "</td><td class='red'>" + item.deaths + "</td></tr>";
+        $('#us .tbody-data').append(strTR);
+    });
+    $('#us .confirmed').text(resultUSA.confirmed.value);  
+    $('#us .recovered').text(resultUSA.recovered.value);  
+    $('#us .deaths').text(resultUSA.deaths.value);         
+    $('#us .lastUpdated').text(formatDateTime(new Date(resultUSA.lastUpdate)));    
+
+    sortedResultChina.forEach(function(item) {
+        strTR = "<tr><td>" + item.provinceState + "</td><td>" + item.confirmed + "</td><td class='red'>" + item.deaths + "</td><td>" + item.recovered + "</td></tr>";
+        $('#china .tbody-data').append(strTR);
+    });
+    $('#china .confirmed').text(resultChina.confirmed.value);  
+    $('#china .recovered').text(resultChina.recovered.value);  
+    $('#china .deaths').text(resultChina.deaths.value);         
+    $('#china .lastUpdated').text(formatDateTime(new Date(resultChina.lastUpdate)));    
+  })
+})
